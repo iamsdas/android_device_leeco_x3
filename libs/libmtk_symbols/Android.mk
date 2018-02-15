e302ff50b600390370dfc357dfd52efefdb6a694
vishalk17 <vishalkapadi95@gmail.com>
2018-02-15 17:28:23 +0000

    libmtk_symbol: update Android.mk
    it may fix- cannot locate symbol "__xlog_buf_printf" referenced by "/system/lib64/libdpframework.so

--- libs/libmtk_symbols/Android.mk
+++ libs/libmtk_symbols/Android.mk
@@ -18,8 +18,8 @@ LOCAL_SRC_FILES_32 += mtk_wvm32.cpp
 LOCAL_SRC_FILES_64 := mtk_parcel.cpp
 LOCAL_SRC_FILES_64 += mtk_wvm64.cpp
 
+LOCAL_SHARED_LIBRARIES := libdpframework libcutils libbinder libutils liblog libgui libui libicuuc libicui18n libcrypto libstagefright_foundation libssl
 
-LOCAL_SHARED_LIBRARIES := libbinder libutils liblog libgui libui libicuuc libicui18n libcrypto libstagefright_foundation libssl
 LOCAL_C_INCLUDES += frameworks/av/media/mtp/ system/core/include/ frameworks/rs/server/ frameworks/av/include/ hardware/libhardware/include/
 LOCAL_MODULE := libmtk_symbols
 LOCAL_MODULE_TAGS := optional
