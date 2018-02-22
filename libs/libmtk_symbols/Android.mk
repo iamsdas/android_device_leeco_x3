3ea2378d633da72a2570eb1d49c3b7182f7bff32
vishalk17 <vishalkapadi95@gmail.com>
2018-02-22 19:03:51 +0000

    add missing symbol to shim

--- libs/libmtk_symbols/Android.mk
+++ libs/libmtk_symbols/Android.mk
@@ -7,6 +7,7 @@ LOCAL_SRC_FILES := \
 	mtkaudio.cpp  \
 	mtk_ui.cpp \
 	mtk_omx.cpp \
+        mtk_sensor.cpp \
 	icu55.c \
 	crypto.c \
 	ssl.c
@@ -18,9 +19,9 @@ LOCAL_SRC_FILES_32 += mtk_wvm32.cpp
 LOCAL_SRC_FILES_64 := mtk_parcel.cpp
 LOCAL_SRC_FILES_64 += mtk_wvm64.cpp
 
-LOCAL_SHARED_LIBRARIES := libdpframework libcutils libbinder libutils liblog libgui libui libicuuc libicui18n libcrypto libstagefright_foundation libssl
+LOCAL_SHARED_LIBRARIES := libdpframework libcutils libbinder libutils liblog libgui libui libicuuc libicui18n libcrypto libstagefright_foundation libssl libsensor
+LOCAL_C_INCLUDES += frameworks/av/media/mtp/ system/core/include/ frameworks/rs/server/ frameworks/av/include/ hardware/libhardware/include/ frameworks/native/libs/sensor/include/
 
-LOCAL_C_INCLUDES += frameworks/av/media/mtp/ system/core/include/ frameworks/rs/server/ frameworks/av/include/ hardware/libhardware/include/
 LOCAL_MODULE := libmtk_symbols
 LOCAL_MODULE_TAGS := optional
 
